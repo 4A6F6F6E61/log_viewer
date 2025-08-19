8437a5ead3fb3a3246667b08351270e3ce649797
Joona Brückner <joona.brueckner@gmail.com>
2025-08-19 22:32:56 +0200

    remove mobile & web

--- android/app/src/main/kotlin/com/example/log_viewer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.log_viewer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/window/window.dart
+++ lib/window/window.dart
@@ -4,6 +4,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart';
 import 'package:log_viewer/window/window_title_bar.dart';
 
+// TODO: Move Window code into seperate Package, so it can be easily reused in other projects
 class AppWindow extends StatelessWidget {
   const AppWindow({super.key, required this.child});
 
